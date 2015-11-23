5be546d8c85748edca4bc618cf7908ecf4d3e475
anthony-toich <atoich@masonlive.gmu.edu>
2015-11-22 21:06:36 -0800

    Added basic controller, index page.

--- config/routes.rb
+++ config/routes.rb
@@ -53,4 +53,7 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+
+get "/wines" => "wines#index", as: 'wines'
+
 end
