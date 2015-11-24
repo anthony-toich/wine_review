eb73bcd40018bddac5933be03ddf4b119814eb6c
anthony-toich <atoich@masonlive.gmu.edu>
2015-11-23 18:59:00 -0800

    Added edit and create pages

--- app/controllers/wines_controller.rb
+++ app/controllers/wines_controller.rb
@@ -7,7 +7,29 @@ class WinesController < ApplicationController
     @wine = Wine.find(params[:id])
   end
 
+def new
+  @wine = Wine.new
+end
 
+def create
+  @wine = Wine.new(wine_params)
+  @wine.save
+  redirect_to @wine
+end
 
+def edit
+  @wine = Wine.find(params[:id])
+end
+
+def update
+  @wine = Wine.find(params[:id])
+  @wine.update(wine_params)
+  redirect_to @wine
+end
+
+private
+def wine_params
+  params.require(:wine).permit(:name, :year, :winery, :country, :varietal)
+end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -55,5 +55,10 @@ Rails.application.routes.draw do
   #   end
 
 get "/wines" => "wines#index", as: 'wines'
+get "/wines/new" => "wines#new", as: 'new_wine'
 get "/wines/:id" => "wines#show", as: 'wine'
+post "/wines" => "wines#create"
+get "/wines/:id/edit" => "wines#edit", as: 'edit_wine'
+patch "/wines/:id" => "wines#update"
+
 end
