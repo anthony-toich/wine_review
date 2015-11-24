648c289d1882a19242917b82cc6c48d44e150b1c
anthony-toich <atoich@masonlive.gmu.edu>
2015-11-23 17:14:22 -0800

    Added data through seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Wine.create(name:'First Wine', winery:'Gallo Vineyards', country:'US', varietal:'Chardonay')
+Wine.create(name:'Second Wine', winery:'Gallo Vineyards', country:'US', varietal:'Rose')
+Wine.create(name:'Third Wine', winery:'Gallo Vineyards', country:'US', varietal:'Chablis')
+Wine.create(name:'Hair of the Dog', winery:'Soothsayer Vineyards', country:'CA', varietal:'Reisling')
+Wine.create(name:'Bloody Monday', winery:'Soothsayer Vineyards', country:'CA', varietal:'Burgundy')
+Wine.create(name:'Lost Cause', winery:'Soothsayer Vineyards', country:'CA', varietal:'Zinfendel')
+Wine.create(name:'Wild Time', winery:'Grapes United', country:'GB', varietal:'Rose')
+Wine.create(name:'Summer Fling', winery:'Grapes United', country:'GB', varietal:'Chianti')
+Wine.create(name:'Last Dregs', winery:'Grapes United', country:'GB', varietal:'Burgundy')
