0400ab870c733f41221dd13193f60abd123a0cc5
anthony-toich <atoich@masonlive.gmu.edu>
2015-11-29 18:41:41 -0800

    Added select box and validation code.

--- app/models/wine.rb
+++ app/models/wine.rb
@@ -1,4 +1,8 @@
 class Wine < ActiveRecord::Base
+  VARIETALS = [ 'Burgundy', 'Chablis', 'Chianti', 'Chardonay', 'Reisling', 'Rose', 'Zinfendel']
   validates :name, :year, :country, :varietal, :winery, presence: true
   validates :year, numericality: {only_integer: true, greater_than_or_equal_to: 1500}
+  validates :varietal,
+    :inclusion => { :in => VARIETALS,
+    :message => "%{value} is not a valid varietal."}
 end
